b10df76ccd379148dc423bb1a623aa58ca66473d
Rowell Heria <owieh@me.com>
2017-06-08 16:45:16 +0100

    capitalise functionality and test added

--- src/value-converters/capitalise.ts
+++ src/value-converters/capitalise.ts
@@ -1,5 +1,19 @@
 export class CapitaliseValueConverter {
-  toView(value) {
-    return value;
+  toView(value: string) {
+    if (value === null || value === undefined || value.length === 0) {
+      return value;
+    }
+
+    return value.toString()
+      .replace(/(?:^|\s|-)\S/g, (char) => char.toUpperCase());
+  }
+
+  fromView(value: string) {
+    if (value === null || value === undefined || value.length === 0) {
+      return value;
+    }
+
+    return value.toString()
+      .replace(/(?:^|\s|-)\S/g, (char) => char.toUpperCase());
   }
 }
--- test/unit-test/value-converters/capitalise.spec.ts
+++ test/unit-test/value-converters/capitalise.spec.ts
@@ -7,7 +7,24 @@ describe("CapitaliseValueConverter", () => {
     sut = new CapitaliseValueConverter();
   });
 
-  it("retuens the same value", () => {
-    expect(sut.toView("test")).toEqual("test");
+  describe("toView", () => {
+    it("returns capitalised single word", () => {
+      expect(sut.toView("word")).toEqual("Word");
+      expect(sut.toView("wOrd")).toEqual("WOrd");
+    });
+
+    it("returns capitalised multiple words", () => {
+      expect(sut.toView("words words")).toEqual("Words Words");
+    });
+
+    it("returns null / undefined / empty string", () => {
+      expect(sut.toView(null)).toEqual(null);
+      expect(sut.toView(undefined)).toEqual(undefined);
+      expect(sut.toView("")).toEqual("");
+    });
+  });
+
+  describe("fromView", () => {
+    
   });
 });
